cd2ea5855064c3034a830bf0ea4b81cf95e214d7
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-02 17:28:20 +0530

    Update urls.py

--- bcvDjangoBackend/bcvDjangoBackend/urls.py
+++ bcvDjangoBackend/bcvDjangoBackend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bcvDjangoBackend project.
+URL configuration for business_contract_validator project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,14 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , views.home , name='home'),
+    path('' , include('pdfform.urls')),
     path('pdf_highlighter/', views.pdf_highlighter , name='pdf_highlighter'),
-    path('pdf_summary/', views.pdf_summary , name='pdf_summary'),
+    path('pdf_summary/', views.pdf_summury , name='pdf_summary'),
 ]
-
 
