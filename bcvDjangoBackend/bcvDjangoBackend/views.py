6bc634c5ce010ea0e4ac5c4c8da4d90979051e1b
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-02 11:18:13 +0530

    Update views.py

--- bcvDjangoBackend/bcvDjangoBackend/views.py
+++ bcvDjangoBackend/bcvDjangoBackend/views.py
@@ -1,1 +1,11 @@
 
+from django.http import HttpResponse
+
+def Pdf_highlighter(request):
+    return HttpResponse("the component is for PDF highlight" )
+
+def Pdf_summury(request):
+    return HttpResponse("the component is for PDF summury" )
+
+def home(request):
+    return HttpResponse("this home page" )
