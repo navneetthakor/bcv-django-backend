442bce38cdd759ff31b4e8903211a4255fa43e75
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-02 17:30:04 +0530

    Add files via upload

---
+++ bcvDjangoBackend/bcvDjangoBackend/pdfform/forms.py
@@ -0,0 +1,6 @@
+# file_upload_app/forms.py
+
+from django import forms
+
+class FileUploadForm(forms.Form):
+    file = forms.FileField()
--- bcvDjangoBackend/bcvDjangoBackend/pdfform/urls.py
+++ bcvDjangoBackend/bcvDjangoBackend/pdfform/urls.py
@@ -1,1 +1,8 @@
+# file_upload_app/urls.py
 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.file_upload_view, name='file_upload'),
+]
