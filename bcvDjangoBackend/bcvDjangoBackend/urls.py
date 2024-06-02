d7d60eacc815e40715357113cb86f0bd04b5f0df
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-02 11:17:34 +0530

    Update urls.py

--- bcvDjangoBackend/bcvDjangoBackend/urls.py
+++ bcvDjangoBackend/bcvDjangoBackend/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , views.home , name='home'),
+    path('Pdf_highlighter/', views.Pdf_highlighter , name='Pdf_highlighter'),
+    path('Pdf_summury/', views.Pdf_summury , name='Pdf_summury'),
 ]
+
+
