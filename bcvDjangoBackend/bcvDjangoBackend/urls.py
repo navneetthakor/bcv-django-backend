eb872236bb522ec08b4ecf2b57f5f761d45dec7b
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-02 11:26:33 +0530

    Update urls.py

--- bcvDjangoBackend/bcvDjangoBackend/urls.py
+++ bcvDjangoBackend/bcvDjangoBackend/urls.py
@@ -21,8 +21,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.home , name='home'),
-    path('Pdf_highlighter/', views.Pdf_highlighter , name='Pdf_highlighter'),
-    path('Pdf_summury/', views.Pdf_summury , name='Pdf_summury'),
+    path('pdf_highlighter/', views.pdf_highlighter , name='pdf_highlighter'),
+    path('pdf_summary/', views.pdf_summary , name='pdf_summary'),
 ]
 
 
