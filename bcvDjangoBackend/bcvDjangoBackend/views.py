fe10aafe8c70fe7ed6431f88d8bdc55c1e1c861b
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-04 21:43:12 +0530

    Update views.py

--- bcvDjangoBackend/bcvDjangoBackend/views.py
+++ bcvDjangoBackend/bcvDjangoBackend/views.py
@@ -5,24 +5,27 @@ import cloudinary
 import cloudinary.uploader
 import requests
 from cloudinary.utils import cloudinary_url
+import urllib.parse
 
-
-
+# The cloudinary credential
 cloudinary.config(
     cloud_name='dzlv9zrk8',
     api_key='689549637748837',
     api_secret='8FaQn5CszbftFojnsUnPUN0Z7tM'
 )
 
-
 def pdf_highlighter(request):
      if request.method == 'GET':
         # Path to the local PDF file
-        file_path = "D:/Ronak/books/knowledge_book/ArtOfWar.pdf"
+        file_path = "D:/Ronak/intel project/CUAD_v1/CUAD_v1/full_contract_pdf/Part_I/Affiliate_Agreements/CreditcardscomInc_20070810_S-1_EX-10.33_362297_EX-10.33_Affiliate Agreement.pdf"
+
+         # Fetching the filename
+        parsed_url = urllib.parse.urlparse(file_path)
+        filename = parsed_url.path.split("/")[-1]
 
         try:
-            # Upload the file to Cloudinary
-            result = cloudinary.uploader.upload(file_path, resource_type="raw")
+            # Upload the file to Cloudinary with filename
+            result = cloudinary.uploader.upload(file_path, public_id = filename, resource_type="raw")
 
             # Get the URL of the uploaded file
             temp_url = result['secure_url']
@@ -43,17 +46,24 @@ def home(request):
 
 
 def download_pdf(public_id):
-    file_name = "test.pdf"
-
     try:
 
         resource_type = 'raw'
+
+        # Creating URL to make request on
         signature = cloudinary_url(public_id, resource_type=resource_type)[0]
 
+        # making request
         response = requests.get(signature, stream=True)
 
+        # Fetching the name of the file
+        parsed_url = urllib.parse.urlparse(public_id)
+        filename = parsed_url.path.split("/")[-1]
+        path =  "D:\\Ronak\\intel_back_end_django\\bcv_backend\\static\\" + filename 
+
         if response.status_code == 200:
-            with open('D:/Ronak/intel_back_end_django/bcv_backend/static/my_downloaded_file.pdf', 'wb') as f:
+            # Storing the file locally
+            with open(path , 'wb') as f:
                 for chunk in response.iter_content(1024):
                     f.write(chunk)
             return "succesfull"
@@ -66,4 +76,5 @@ def download_pdf(public_id):
         return  f"Timeout Error: {errt}"
     except requests.exceptions.RequestException as err:
         return  f"Something went wrong: {err}"
+
 
