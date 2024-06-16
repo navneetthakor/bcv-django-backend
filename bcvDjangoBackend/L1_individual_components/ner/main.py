5cd4854dc98242b184f17120deadcf686acbb840
Rohanshu-33 <banodharohanshu@gmail.com>
2024-06-16 12:19:41 +0530

    additions in ner, pdfparser

--- bcvDjangoBackend/L1_individual_components/ner/main.py
+++ bcvDjangoBackend/L1_individual_components/ner/main.py
@@ -4,7 +4,9 @@ from ner import Ner
 nerInstance = Ner("This agreement holds between Apple INC.  and Samsung INC. for a joint venture for 6 months for a total budget of $2,00,000.")
 
 # NER main function for making entity relations
-nerInstance.ner()
+ner_dict = nerInstance.ner()
 
 # NER method for printing the entities
 nerInstance.printNER()
+
+print(ner_dict)
