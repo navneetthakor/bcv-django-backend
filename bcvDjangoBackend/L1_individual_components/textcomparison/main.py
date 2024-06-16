8269ab84827cb266eb73b828abd7d26d6a899aed
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-16 11:21:32 +0530

    Update main.py

--- bcvDjangoBackend/L1_individual_components/textcomparison/main.py
+++ bcvDjangoBackend/L1_individual_components/textcomparison/main.py
@@ -1,7 +1,14 @@
 from textcomparison import TextComparison
+from textclassifier import TextClassifier
 
 # Instantiating an NER instance of  Ner class
-comparisonInstance = TextComparison("text")
+classifyInstance = TextClassifier(pdfPath , ContractType)
+
+# NER main function for making entity relations
+paragraphs = classifyInstance.classify(pdfType , ContractType)
+
+# Instantiating an NER instance of  Ner class
+comparisonInstance = TextComparison(paragraphs)
 
 # NER main function for making entity relations
 comparisonInstance.comparator()
