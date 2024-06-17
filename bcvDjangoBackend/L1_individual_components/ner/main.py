32a0e11a203acdbe703dafb5e8b6ae011fc0b250
Rohanshu-33 <banodharohanshu@gmail.com>
2024-06-17 11:27:57 +0530

    changed ner model. using directly huggingface api

--- bcvDjangoBackend/L1_individual_components/ner/main.py
+++ bcvDjangoBackend/L1_individual_components/ner/main.py
@@ -1,12 +1,12 @@
 from ner import Ner
 
 # Instantiating an NER instance of  Ner class
-nerInstance = Ner("This agreement holds between Apple INC.  and Samsung INC. for a joint venture for 6 months for a total budget of $2,00,000.")
+nerInstance = Ner("hlo")
 
 # NER main function for making entity relations
-ner_dict = nerInstance.ner()
+nerInstance.ner()
 
 # NER method for printing the entities
-nerInstance.printNER()
+ner_dict = nerInstance.printNER()
 
 print(ner_dict)
--- bcvDjangoBackend/L1_individual_components/ner/ner.py
+++ bcvDjangoBackend/L1_individual_components/ner/ner.py
@@ -1,35 +1,36 @@
-from flair.data import Sentence
-from flair.models import SequenceTagger
+import requests
 
-# load tagger
-tagger = SequenceTagger.load("flair/ner-english-ontonotes-large")
+API_URL = "https://api-inference.huggingface.co/models/flair/ner-english-ontonotes-large"
+headers = {"Authorization": "Bearer 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"}
 
 class Ner:
   def __init__(self, pdfText):
     self.text = pdfText
     self.sentence = ""
+    self.output=[]
 
   def ner(self):
     try:
       print("Starting NER Task.....")
-      self.sentence = Sentence(self.text)
-
-      tagger.predict(self.sentence)
-
-      # Create dictionary of entities
-      entities = {}
-      for entity in self.sentence.get_spans('ner'):
-          entities[entity.text] = [entity.get_label('ner').value, entity.score]
-      return entities
+      self.output = self.query({
+	    "inputs": f"{self.text}"})
+      
+      print("NER performed successfully.")
       
     except Exception as err:
       print(f"Error occurred while reading pdf : {err}")
-      return {}
 
   def printNER(self):
     
     print('The following NER tags are found:')
+    ner_dict = {}
+    for dicts in self.output:
+      ner_dict[dicts["word"]] = [dicts["entity_group"], dicts["score"]]
+    
+    return ner_dict
+
       
-    # iterate over entities and print
-    for entity in self.sentence.get_spans('ner'):
-      print(entity)
+  @staticmethod
+  def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
