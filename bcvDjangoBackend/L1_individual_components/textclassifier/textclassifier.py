453463c2401c0d408fd2e63e78884ceb1fc5a1e0
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-19 13:34:01 +0530

    Update textclassifier.py

--- bcvDjangoBackend/L1_individual_components/textclassifier/textclassifier.py
+++ bcvDjangoBackend/L1_individual_components/textclassifier/textclassifier.py
@@ -1,7 +1,9 @@
-from pdfminer.high_level import extract_text, extract_pages
+from pdfminer.high_level import extract_text
+import re
 import json
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTTextLine, LTChar
+import os
 
 class TextClassifier:
   
@@ -30,7 +32,7 @@ class TextClassifier:
 
     return paragraphs
 
-  def type2_classify(pdf_path, headings):
+  def Type2_classify(pdf_path, headings):
     paragraphs = {}
     current_heading = None
 
@@ -58,24 +60,27 @@ class TextClassifier:
       type1 = ['Beta Test AgreeMent' , 'Influencer Agreement',]
       type2 = ['Default' , 'Franchise Agreement' , 'Joint Venture Agreement' , 'License Agreement']
 
-      with open('./BusinessContractValidation.templates.json') as f:
-        data = json.load(f)
+      for files in os.listdir('./L1_individual_components/textclassifier'):
+         print(files)
+
+      # just to find the path
+      # with open('./L1_individual_components/textclassifier/templates.json') as f:
+      #   data = json.load(f)
 
       heading = []
-
       for i in data:
-        if i['agreeType'] == self.type :
+        if i['agreeType'] == self.ContractType :
           for clause in i['clauses']:
             heading.append(clause)
 
       if type in type1 :
-        self.paragraphs = self.Type1_classify(self.pdfPath, heading)
+        self.paragraphs = Type1_classify(self.pdfPath, heading)
       else :
-        self.paragraphs = self.type2_classify(self.pdfPath, heading)
+        self.paragraphs = Type2_classify(self.pdfPath, heading)
         
         
       print("dummy text classifier method")
-      return self.paragraphs
+      return paragraphs
     except Exception as err:
       print(f"Error occured while classifying text : {err}")
 
