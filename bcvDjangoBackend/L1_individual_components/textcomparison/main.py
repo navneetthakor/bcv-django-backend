445637c312d27cdab98fb2c0a241951130426a7a
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-06-26 11:00:53 +0530

    Update main.py

--- bcvDjangoBackend/L1_individual_components/textcomparison/main.py
+++ bcvDjangoBackend/L1_individual_components/textcomparison/main.py
@@ -1,19 +1,10 @@
 from textcomparison import TextComparison
-from textclassifier.textclassifier import TextClassifier
 import os
 import json
 import google.generativeai as genai
 
 
 def main():
-
-    # Instantiating an NER instance of  Ner class
-    classifyInstance1 = TextClassifier(pdfPath_template , ContractType)
-    classifyInstance2 = TextClassifier(pdf_path , ContractType)
-
-    # NER main function for making entity relations
-    paragraphs_template = classifyInstance1.classify()
-    paragraphs_contract = classifyInstance2.classify()
 
     # Instantiating an NER instance of  Ner class
     comparisonInstance = TextComparison(paragraphs_template ,paragraphs_contract)
